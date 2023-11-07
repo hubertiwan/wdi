66aa2a3a499b0cfb6e8cc6452a3d1d43f2bc71c7
Hubert Iwanowski <148163250+hubertiwan@users.noreply.github.com>
2023-11-07 16:11:23 +0100

    zestaw2_zad7.py

--- zestaw2/zestaw2_zad7.py
+++ zestaw2/zestaw2_zad7.py
@@ -1,27 +1,33 @@
 #program wypisujacy silnie liczb z zakresu od 1 do M, gdzie M jest liczba podana przez uzytkownika
 
-import os,time
-global factorial, tempFactorial
+import os
 
 def FactorialN(endOfRangeF):
-    tempFactorial = 1
     factorials = []
     for i in range(1, endOfRangeF + 1):# liczby od 1 do konca zakresu podanego przez uzytkownika
         factorial = 1       
-        for j in range(1, i + 1): #liczenie silnii poprzez mnozenie liczb od 1 do i wlacznie
+        for j in range(1, i + 1):   # liczenie silni poprzez mnozenie liczb od 1 do i wlacznie
             factorial *= j
         print("Silnia liczby ", i, " wynosi: ", factorial)
         factorials.append(factorial)
-    for i in range(1, len(factorials)): #podany zakres bo dla 'i in factorials' wychodzimy poza zakres
+    for i in range(1, len(factorials)):
         print("Roznica miedzy silnia z liczby ", i+1, " a silnia z liczby ", i, " wynosi: ", factorials[i]-factorials[i-1])
     
 def CheckIfCorrect(endOfRangeF):
     while endOfRangeF <= 0 or endOfRangeF > 10000:
-        endOfRangeF = int(input("Podaj inna liczbe z zakresu [1, 10000]: "))
-        #CheckIfCorrect(endOfRangeF1)
-        time.sleep(0.2)
-        os.system('cls')
+        os.system('clear')
+        endOfRangeF = int(input("Podaj inna liczbe z zakresu [1, 10000]: "))        
     FactorialN(endOfRangeF)        
         
-endOfRange = int(input("Podaj liczbe, do ktorej zostanie policzona silnia: "))#maksymalna liczba dla ktorej nie pojawi sie blad to 1558
+endOfRange = int(input("Podaj liczbe, do ktorej zostanie policzona silnia: "))  #maksymalna liczba dla ktorej nie pojawi sie blad to 1558
 CheckIfCorrect(endOfRange)
+
+'''
+Silnia liczby  1  wynosi:  1
+Silnia liczby  2  wynosi:  2
+Silnia liczby  3  wynosi:  6
+Silnia liczby  4  wynosi:  24
+Roznica miedzy silnia z liczby  2  a silnia z liczby  1  wynosi:  1
+Roznica miedzy silnia z liczby  3  a silnia z liczby  2  wynosi:  4
+Roznica miedzy silnia z liczby  4  a silnia z liczby  3  wynosi:  18
+'''

d9c3b0dcc6c6f6d2e52d39c489b2ba9e4ec56e9a
Hubert Iwanowski <148163250+hubertiwan@users.noreply.github.com>
2023-11-19 20:36:49 +0100

    Add files via upload

---
+++ zestaw3/zestaw3_zad11.py
@@ -0,0 +1,82 @@
+# program wczytujacy tablice dwuwymiarowa o wymiarach n x n, gdzie n jest podane przez uzytkownika
+# program uzupełnia tablice losowymi liczbami naturalnymi
+# program zwraca liczbe par elementow o podanym iloczynie odleglych od siebie w tablicy o jeden ruch skoczka szachowego
+
+import random
+
+global size
+size = 0
+board = []
+
+def SetBoardSize():
+    global board
+    global size
+    size = int(input("Podaj wielkosc tablicy: "))
+    board = [[None]*size for _ in range(size)] # wypelnia wartoscia 'None' size-razy, tworzy size takich list -> tablica dwuwymiarowa; '_' oznacza zmienna zastepcza, nieuzywana pozniej
+
+def GeneratingNumbers():
+    global board
+    for i in range(len(board)):
+        for j in range(len(board)):
+            board[i][j] = random.randint(0, 5)            
+
+def PrintBoard():    
+    for i in range(len(board)):  
+        for j in range(len(board[i])):
+            if board[i][j] < 10:
+                print(board[i][j], end="  ")      
+            else:
+                print(board[i][j], end=" ") 
+        print()  
+    
+def CountingPair(): # dodac warunek zeby pary liczb sie nie powtarzaly
+    product = int(input("Podaj iloczyn, ktory chcesz sprawdzic: "))
+    pairsSum = 0
+    length = len(board)
+        
+    for i in range(length): # petle z 'i' i 'j' trzymaja jeden punkt, a z 'x' i 'y' ida po calej tablicy sprawdzajc warunki z dwóch if-ow
+        for j in range(length):
+            for x in range(length):
+                for y in range(length):
+                    if i != x and j != y:
+                        if abs(i-x) * abs(j-y) == 2 and board[i][j] * board[x][y] == product: # abs(i-x) * abs(j-y) == 2 --> skoczek, dwa do przodu, jeden w bok
+                            pairsSum += 1
+    pairsSum = pairsSum // 2 # liczba policzonych par jest podwojna            
+    print(f"Liczba par elementow o iloczynie {product}, odleglych od siebie o jeden ruch skoczka szachowego wynosi: {pairsSum}")
+        
+SetBoardSize()
+GeneratingNumbers()
+PrintBoard()
+CountingPair()
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] * board[i+2][j-1] == product:
+                pairsSum += 1
+            elif board[i][j] * board[i+2][j+1] == product:
+                pairsSum += 1
+            elif board[i][j] * board[i-1][j+2] == product:
+                pairsSum += 1
+            elif board[i][j] * board[i+1][j+2] == product:
+                pairsSum += 1 
+            elif board[i][j] * board[i-2][j-1] == product:
+                pairsSum += 1
+            elif board[i][j] * board[i-2][j+1] == product:
+                pairsSum += 1
+            elif board[i][j] * board[i-1][j-2] == product:
+                pairsSum += 1
+            elif board[i][j] * board[i+1][j-2] == product:
+                pairsSum += 1 
+'''
